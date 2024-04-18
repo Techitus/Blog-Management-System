b7e05a54d17deff2eff4c67863a6048582a257eb
Kamal Paudel <techitus07@gmail.com>
2024-04-18 17:59:21 +0545

    Auth completed

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,19 +1,22 @@
-import { data } from "autoprefixer";
 import Form from "./components/Form/Form";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";                                                                                                 
 import { baseUrl } from "../../config";
 const Login = () => {
   const navigate = useNavigate();
-  const handleLogin = async () => {
+  const handleLogin = async (data) => {
     try {
-      const response = await axios.get(
+      const response = await axios.post(
         `${baseUrl}/login`,
         data
       );
+      console.log(response)
       if (response.status === 200) {
         localStorage.setItem('token', response.data.token);
         navigate("/");
+      }
+      else{
+        alert('Login failed')
       }
     } catch (error) {
       alert(error?.response?.data?.message);
