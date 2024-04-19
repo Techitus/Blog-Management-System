6910d53110020d2ad70c5658270af96d7af5faf7
Kamal Paudel <techitus07@gmail.com>
2024-04-19 19:32:01 +0545

    api integrate

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react"
 import Layout from "../../components/layout/Layout"
 import Card from "./components/card/Card"
+import axios from "axios"
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([])
+  const fetchBlogs = async ()=>{
+  const response =  await axios.get(`{baseUrl}/blog`)
+if(response.status === 200){
+  setBlogs(response.data.data)
+}  }
+  useEffect(()=>{
+fetchBlogs()
+  },[])
+
   return (
 <Layout>
     <div className="flex flex-wrap justify-center mt-6 space-x-10">
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
+      {
+        blogs.length > 0 && blogs.map ((blog)=>{
+          return (
+          <Card key={blog.id} blog={blog}/>
+
+          )
+
+        })
+      }
 
     </div>
     
--- src/pages/blog/components/card/Card.jsx
+++ src/pages/blog/components/card/Card.jsx
@@ -1,21 +1,21 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
 
-const Card = () => {
+const Card = ({blog}) => {
   return (
     <>
     <Link to = "/blog/id"/>
     <div className="  max-w-sm rounded overflow-hidden shadow-lg mb-10 space-x-10">
-  <img className="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSQKaS7LP80SEcKgz9-d_ORjkh1B9hPSUqkeI_mLSnDg&s" alt="Sunset in the mountains"/>
+  <img className="w-full" src="blog.imageUrl" alt="image"/>
   <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <div className="font-bold text-xl mb-2">{blog.title}</div>
     <p className="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      { blog.description}
     </p>
   </div>
   <div className="px-6 pt-4 pb-2">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{blog.catagory}</span>
+    
   </div>
 </div>
     </>
